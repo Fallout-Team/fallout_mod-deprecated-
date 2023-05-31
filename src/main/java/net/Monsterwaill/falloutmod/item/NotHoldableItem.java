ce09eb371064ae3ae196e6514cc2df069963e2f0
Duzo <james.hall01@outlook.com>
2023-05-31 22:01:32 +0100

    Items that cant exist, yup.

---
+++ src/main/java/net/Monsterwaill/falloutmod/item/NotHoldableItem.java
@@ -0,0 +1,20 @@
+package net.Monsterwaill.falloutmod.item;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class NotHoldableItem extends Item {
+    public NotHoldableItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, int p_41407_, boolean p_41408_) {
+        super.inventoryTick(stack, level, entity, p_41407_, p_41408_);
+
+        // On exist
+        stack.shrink(stack.getCount()); // Dont.
+    }
+}
