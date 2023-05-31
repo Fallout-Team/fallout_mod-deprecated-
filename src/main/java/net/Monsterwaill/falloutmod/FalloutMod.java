e4efe7a66f91f63382162267b05d6e50f474a2be
Monsterwaill <morganhickingbotham3@gmail.com>
2023-05-31 19:07:42 +0100

    Commiting to test if git in discord is working.

--- src/main/java/net/Monsterwaill/falloutmod/FalloutMod.java
+++ src/main/java/net/Monsterwaill/falloutmod/FalloutMod.java
@@ -15,7 +15,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(FalloutMod.MOD_ID)
+@Mod(FalloutdMod.MOD_ID)
 public class FalloutMod {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "falloutmod";
