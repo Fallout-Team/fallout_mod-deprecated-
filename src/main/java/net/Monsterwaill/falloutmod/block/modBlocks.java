c3213c4ea5e82bf91c4e03817792b4af084ec241
Monsterwaill <morganhickingbotham3@gmail.com>
2023-06-01 10:55:16 +0100

    d

--- src/main/java/net/Monsterwaill/falloutmod/block/modBlocks.java
+++ src/main/java/net/Monsterwaill/falloutmod/block/modBlocks.java
@@ -6,20 +6,29 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import net.Monsterwaill.falloutmod.item.modCreativeModeTab;
 
 import java.util.function.Supplier;
 
 public class modBlocks {
 
-    public static final RegistryObject<Block> RADIO = registerBlock("radio",
+
+    private  static  <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+
+    }
+
+    public static final RegistryObject<Block> RADIO = registerBlockItem("radio",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL)));
+
 
 
     public static final DeferredRegister<Block> BLOCKS =
@@ -30,12 +39,6 @@ public class modBlocks {
         return modItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
     }
 
-    private  static  <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block) {
-        RegistryObject<T> toReturn = BLOCKS.register(name, block);
-        registerBlockItem(name, toReturn, tab);
-        return toReturn;
-
-    }
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
