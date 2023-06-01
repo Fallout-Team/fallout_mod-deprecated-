54645f7d68d0058ac8e9538cfbe7fb23cbea99b4
Monsterwaill <morganhickingbotham3@gmail.com>
2023-06-01 22:10:14 +0100

    Added mineable tags and fixed mining bug

--- src/main/java/net/Monsterwaill/falloutmod/block/FalloutBlocks.java
+++ src/main/java/net/Monsterwaill/falloutmod/block/FalloutBlocks.java
@@ -3,15 +3,12 @@ package net.Monsterwaill.falloutmod.block;
 import net.Monsterwaill.falloutmod.FalloutMod;
 import net.Monsterwaill.falloutmod.item.FalloutItems;
 import net.Monsterwaill.falloutmod.item.FalloutTabs;
-import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -23,7 +20,7 @@ public class FalloutBlocks {
 
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, FalloutMod.MOD_ID);
-    public static final RegistryObject<Block> RADIO = register("radio", () -> new RadioBlock(BlockBehaviour.Properties.of(Material.METAL).noOcclusion()));
+    public static final RegistryObject<Block> RADIO = register("radio", () -> new RadioBlock(BlockBehaviour.Properties.of(Material.METAL).noOcclusion().strength(4).requiresCorrectToolForDrops()));
 
 
     /**
