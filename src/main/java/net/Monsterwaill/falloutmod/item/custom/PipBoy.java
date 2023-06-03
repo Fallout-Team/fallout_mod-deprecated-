a0d05feee454880ca3ab2e34d7ed228f6e47b506
Monsterwaill <morganhickingbotham3@gmail.com>
2023-06-03 23:02:04 +0100

    tbh forgot what I added

--- src/main/java/net/Monsterwaill/falloutmod/item/custom/PipBoy.java
+++ src/main/java/net/Monsterwaill/falloutmod/item/custom/PipBoy.java
@@ -1,5 +1,7 @@
 package net.Monsterwaill.falloutmod.item.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
@@ -7,12 +9,27 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class PipBoy extends Item {
 
     public PipBoy(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag tooltipFlag) {
+        if(Screen.hasShiftDown()) {
+            components.add(Component.literal("The Pip-Boy 2000 Mk VI is only operational when held in the offhand slot. For more info please refer to your Pip-Boy manual provided by vault tech.").withStyle(ChatFormatting.GREEN));
+        } else {
+            components.add(Component.literal("Press shift!").withStyle(ChatFormatting.DARK_RED));
+        }
+
+        super.appendHoverText(itemStack, level, components, tooltipFlag);
     }
 
     @Override
